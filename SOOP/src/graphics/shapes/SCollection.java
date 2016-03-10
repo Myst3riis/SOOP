a37294c1182411d3ea4934675ad63d047b88e2bc
thomas_lecointre <thomas.lecointre1@gmail.com>
2016-03-10 15:54:42 +0100

    SCollection presque terminée
    * Ajout des données membres xmin, xmax, ymin, ymax
    * Constructeur par défaut
    * Méthode add mis à jour pour pouvoir ajuster les données membres au
    lors d'un ajouts de formes
    * setLoc gère toutes les formes et collections
    * translate de même

--- SOOP/src/graphics/shapes/SCollection.java
+++ SOOP/src/graphics/shapes/SCollection.java
@@ -7,8 +7,21 @@ import java.util.Iterator;
 
 public class SCollection extends Shape
 {
+	private int xmin;
+	private int ymin;
+	
+	private int xmax;
+	private int ymax;
 
 	private ArrayList<Shape> arr = new ArrayList<Shape>();
+
+	public SCollection()
+	{
+		this.xmin = 0;
+		this.ymin = 0;
+		this.xmax = 0;
+		this.ymax = 0;
+	}
 
 	public Iterator<Shape> iterator()
 	{
@@ -18,40 +31,62 @@ public class SCollection extends Shape
 	public void add(Shape shape)
 	{
 		this.arr.add(shape);
+		
+		Point loc = shape.getLoc();
+		Rectangle rect = shape.getBounds();
+		
+		if(loc.x < this.xmin)
+			this.xmin = loc.x;
+		
+		if(loc.x + rect.width > this.xmax)
+			this.xmax = (loc.x + rect.width);
+		
+		if(loc.y < this.ymin)
+			this.ymin = loc.y;
+		
+		if(loc.y + rect.height > this.ymax)
+			this.ymax = (loc.y + rect.height);
 	}
 
 	@Override
 	public Point getLoc()
 	{
-		// Doit renvoyer le point en haut à gauche de la collection
-		return null;
+		return new Point(this.xmin, this.ymin);
 	}
 
 	@Override
 	public void setLoc(Point loc)
 	{
-		for(Iterator<Shape> it = this.iterator(); it.hasNext();)
+		this.xmax += (loc.x - this.xmin);
+		this.ymax += (loc.y - this.ymin);
+		this.xmin = loc.x;
+		this.ymin = loc.y;
+		
+		for (Iterator<Shape> it = this.iterator(); it.hasNext();)
 			it.next().setLoc(loc);
 	}
 
 	@Override
 	public Rectangle getBounds()
 	{
-		// Doit renvoyer un rectangle englobant la collection
-		return null;
+		return new Rectangle(this.xmin, this.ymin, this.xmax - this.xmin, this.ymax - this.ymin);
 	}
 
 	@Override
 	public void accept(ShapeVisitor sv)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void translate(int dx, int dy)
 	{
-		for(Iterator<Shape> it = this.iterator(); it.hasNext();)
+		this.xmin += dx;
+		this.ymin += dy;
+		this.xmax += dx;
+		this.ymax += dy;
+		
+		for (Iterator<Shape> it = this.iterator(); it.hasNext();)
 			it.next().translate(dx, dy);
 	}
 
