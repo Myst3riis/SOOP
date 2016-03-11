907e7eab5f92a4038d3cd8d939f0c41264d37297
Thomas Lecointre <thomas.lecointre1@gmail.com>
2016-03-11 12:57:51 +0100

    SelectionAttributes terminée?

--- SOOP/src/graphics/shapes/attributes/SelectionAttributes.java
+++ SOOP/src/graphics/shapes/attributes/SelectionAttributes.java
@@ -2,6 +2,8 @@ package graphics.shapes.attributes;
 
 public class SelectionAttributes extends Attributes
 {
+	public static final String ID = "selection";
+	
 	private boolean selected;
 
 	public boolean isSelected()
@@ -11,22 +13,22 @@ public class SelectionAttributes extends Attributes
 
 	public void select()
 	{
-
+		this.selected = true;
 	}
 
 	public void unselect()
 	{
-
+		this.selected = false;
 	}
 
 	public void toggleSelection()
 	{
-
+		this.selected = !this.selected;
 	}
 
 	@Override
 	public String getID()
 	{
-		return null;
+		return this.ID;
 	}
 }
